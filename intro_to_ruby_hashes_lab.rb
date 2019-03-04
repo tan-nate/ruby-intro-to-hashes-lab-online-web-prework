72553b9d478437696a15aca729c40469bde2a3a7
Nate Tan <nktan93@gmail.com>
2019-03-04 22:11:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,7 @@ def monopoly_with_second_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {},
-	    :rent_in_dollars => {
-	      :one_piece_owned => 25
-	    }
+	    :rent_in_dollars => {}
 	  }
 	}
 	
